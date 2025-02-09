12d11ecda4b3eaa81858043de2c35fecdc7f8f97
Sabbir Hossen <Sabbir Hossen>
2025-02-08 22:55:34 -1200

    remove lorem

--- src/app/components/Banner.js
+++ src/app/components/Banner.js
@@ -4,7 +4,7 @@ export default function Banner() {
     <div>
       <h2>hello banner mama</h2>
       <p>hello new arpa banner</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos inventore unde cupiditate alias corrupti. Porro animi, officiis quas harum dolor accusantium accusamus vel fugiat eos quidem ex quos odit temporibus?</p>
+
     </div>
   )
 }
