21b882008444c4a08512e1c79730bd3ddfb4998c
Sabbir Hossen <Sabbir Hossen>
2025-02-08 22:51:36 -1200

    check tailwind

---
+++ src/app/components/Banner.js
@@ -0,0 +1,10 @@
+export default function Banner() {
+
+  return (
+    <div>
+      <h2>hello banner mama</h2>
+      <p>hello new arpa banner</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos inventore unde cupiditate alias corrupti. Porro animi, officiis quas harum dolor accusantium accusamus vel fugiat eos quidem ex quos odit temporibus?</p>
+    </div>
+  )
+}
---
+++ src/app/landingPage/LandingPage.js
@@ -0,0 +1,8 @@
+import Banner from "../components/Banner";
+
+
+export default function LandingPage() {
+  return (
+    <Banner />
+  )
+}
